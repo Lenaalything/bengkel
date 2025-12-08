195191d4401b63e8484c658fab7fed3f07b12728
PanduStar77 <hardayapandu@gmail.com>
2025-12-08 16:04:36 +0700

    Update AbsenController.java
    cek jajal

--- Controller/AbsenController.java
+++ Controller/AbsenController.java
@@ -11,93 +11,91 @@ public class AbsenController {
     public ResultSet res;
     public String sql;
 
-    
+    DefaultTableModel dtm = new DefaultTableModel();
 
     public AbsenController() {
         Koneksi db = new Koneksi();
         db.config();
         this.stm = db.stm;
-
     }
 
+    public DefaultTableModel createTable() {
+        this.dtm.addColumn("ID");
+        this.dtm.addColumn("ID Karyawan");
+        this.dtm.addColumn("Nama");
+        this.dtm.addColumn("Tanggal");
+        return this.dtm;
+    }
 
-    //method tambah absen/innsert
+    public void tampilkanAbsen() {
+        try {
+            this.dtm.getDataVector().removeAllElements();
+            this.dtm.fireTableDataChanged();
+
+            this.sql = "SELECT * FROM tb_absen ORDER BY tanggal_absen DESC";
+            this.res = this.stm.executeQuery(sql);
+
+            while (res.next()) {
+                Object[] obj = new Object[4];
+                obj[0] = res.getInt("id_absen");
+                obj[1] = res.getString("id_karyawan");
+                obj[2] = res.getString("nama");
+                obj[3] = res.getString("tanggal_absen");
+                this.dtm.addRow(obj);
+            }
+        } catch (Exception e) {
+            System.out.println("Gagal query tampilkanAbsen: " + e.getMessage());
+        }
+    }
+
+    // tampilkan absen hanya untuk karyawan tertentu (dipakai saat pegawai login)
+    public void tampilkanAbsenByKaryawan(String idKaryawan) {
+        try {
+            this.dtm.getDataVector().removeAllElements();
+            this.dtm.fireTableDataChanged();
+
+            this.sql = "SELECT * FROM tb_absen WHERE id_karyawan='" + idKaryawan + "' ORDER BY tanggal_absen DESC";
+            this.res = this.stm.executeQuery(sql);
+
+            while (res.next()) {
+                Object[] obj = new Object[4];
+                obj[0] = res.getInt("id_absen");
+                obj[1] = res.getString("id_karyawan");
+                obj[2] = res.getString("nama");
+                obj[3] = res.getString("tanggal_absen");
+                this.dtm.addRow(obj);
+            }
+        } catch (Exception e) {
+            System.out.println("Gagal query tampilkanAbsenByKaryawan: " + e.getMessage());
+        }
+    }
+
     public boolean tambahAbsen(String idKaryawan, String nama) {
-        //hubungkan dengan model
         Absen ab = new Absen();
         ab.setId_karyawan(idKaryawan);
         ab.setNama(nama);
 
         try {
-            this.sql = "INSERT INTO tb_absen (id_karyawan, nama) VALUES ('" + ab.getId_karyawan() + "', '" + ab.getNama() + "')";
-            //untuk insert, update,delete gunakan .executeUpdate(sql)
-
+            // tanggal_absen otomatis di-handle oleh DB (current_timestamp)
+            this.sql = "INSERT INTO tb_absen(id_karyawan,nama) VALUES ('" + ab.getId_karyawan() + "','" + ab.getNama() + "')";
             this.stm.executeUpdate(sql);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
-
     }
 
-    //method tampil riwayat pribadi
-    public DefaultTableModel riwayatAbsen(String idKaryawan) {
-        DefaultTableModel dtmrap = new  DefaultTableModel();
-        
-        dtmrap.addColumn("ID Absen");
-        dtmrap.addColumn("ID Karyawan");
-        dtmrap.addColumn("Nama");
-        dtmrap.addColumn("Tanggal Absen");
-        
-        try{
-            this.sql = "SELECT id_absen, id_karyawan, nama, tanggal_absen FROM tb_absen WHERE id_karyawan = '" + idKaryawan + "' ORDER BY tanggal_absen DESC";
-
-            this.res = this.stm.executeQuery(sql);
-            
-            while (res.next()) {
-                Object[] obj = new Object[4];
-                obj[0] = res.getString("id_absen");
-                obj[1] = res.getString("id_karyawan");
-                obj[2] = res.getString("nama");
-                obj[3] = res.getString("tanggal_absen");
-                dtmrap.addRow(obj);
-            }
-            return dtmrap;
-        }catch (Exception e) {
-            System.out.println("query gagal : " + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public DefaultTableModel  riwayatAbsenSemua() {
-        DefaultTableModel dtmSemua = new DefaultTableModel();
-        
-        dtmSemua.addColumn("ID Absen");
-        dtmSemua.addColumn("ID Karyawan");
-        dtmSemua.addColumn("Nama");
-        dtmSemua.addColumn("Tanggal Absen");
-        
+    public boolean hapusAbsen(int id) {
+        Absen ab = new Absen();
+        ab.setId_absen(id);
         try {
-            // Menampilkan semua data
-            this.sql = "SELECT id_absen, id_karyawan, nama, tanggal_absen FROM tb_absen ORDER BY tanggal_absen DESC";
-
-            this.res = this.stm.executeQuery(sql);
-
-            while (res.next()) {
-                Object[] obj = new Object[4];
-                obj[0] = res.getString("id_absen");
-                obj[1] = res.getString("id_karyawan");
-                obj[2] = res.getString("nama");
-                obj[3] = res.getString("tanggal_absen");
-                dtmSemua.addRow(obj);
-            }
-            return dtmSemua;
+            this.sql = "DELETE FROM tb_absen WHERE id_absen=" + ab.getId_absen();
+            this.stm.executeUpdate(sql);
+            return true;
         } catch (Exception e) {
-            System.out.println("querry gagal " + e.getMessage());
             e.printStackTrace();
-            return null;
+            return false;
         }
     }
 
